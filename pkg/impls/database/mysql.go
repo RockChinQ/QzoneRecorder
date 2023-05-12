0d3aa818272d6a4b1ebb1058e75caed82f99381e
Rock Chin <1010553892@qq.com>
2023-05-12 13:51:31 +0000

    fix: 错误的SQL语句内容

--- pkg/impls/database/mysql.go
+++ pkg/impls/database/mysql.go
@@ -15,10 +15,15 @@ type MySQLAdapter struct {
 	db *sql.DB
 }
 
+func NewMySQLAdapter() *MySQLAdapter {
+	return &MySQLAdapter{}
+}
+
 func (adapter *MySQLAdapter) Connect() error {
+	// fmt.Println(viper.GetString("database.user") + ":" + viper.GetString("database.password") + "@tcp(" + viper.GetString("database.host") + ":" + viper.GetString("database.port") + ")/" + viper.GetString("database.database") + "?charset=utf8mb4")
 	conn, err := sql.Open(
 		"mysql",
-		viper.GetString("database.username")+":"+viper.GetString("database.password")+"@tcp("+viper.GetString("database.host")+":"+viper.GetString("database.port")+")/"+viper.GetString("database.database")+"?charset=utf8mb4&parseTime=True&loc=Local",
+		viper.GetString("database.user")+":"+viper.GetString("database.password")+"@tcp("+viper.GetString("database.host")+":"+viper.GetString("database.port")+")/"+viper.GetString("database.database")+"?charset=utf8mb4",
 	)
 	if err != nil {
 		return err
@@ -30,39 +35,39 @@ func (adapter *MySQLAdapter) Connect() error {
 }
 
 var sqls = []string{
-	`CREATE TABLE IF NOT EXISIS Person (
+	`CREATE TABLE IF NOT EXISTS Person (
 		id BIGINT UNSIGNED NOT NULL AUTO_INCREMENT,
 		qq BIGINT UNSIGNED NOT NULL,
 		nick VARCHAR(255) NOT NULL,
 		PRIMARY KEY (id, qq)
 	)`,
-	`CREATE TABLE IF NOT EXISIS Emotion (
+	`CREATE TABLE IF NOT EXISTS Emotion (
 		id BIGINT UNSIGNED NOT NULL AUTO_INCREMENT,
 		eid VARCHAR(255) NOT NULL,
 		person_id BIGINT UNSIGNED NOT NULL,
 		text TEXT NOT NULL,
 		time_stamp BIGINT UNSIGNED NOT NULL,
-		FROEIGN KEY (person_id) REFERENCES Person(id),
+		FOREIGN KEY (person_id) REFERENCES Person(id),
 		PRIMARY KEY (id, eid)
 	)`,
-	`CREATE TABLE IF NOT EXISIS Image (
+	`CREATE TABLE IF NOT EXISTS Image (
 		id BIGINT UNSIGNED NOT NULL AUTO_INCREMENT,
 		emotion_id BIGINT UNSIGNED NOT NULL,
 		url VARCHAR(1024) NOT NULL,
-		FROEIGN KEY (emotion_id) REFERENCES Emotion(id),
+		FOREIGN KEY (emotion_id) REFERENCES Emotion(id),
 		PRIMARY KEY (id)
 	)`,
-	`CREATE TABLE IF NOT EXISIS Comment (
+	`CREATE TABLE IF NOT EXISTS Comment (
 		id BIGINT UNSIGNED NOT NULL AUTO_INCREMENT,
 		emotion_id BIGINT UNSIGNED NOT NULL,
 		person_qq BIGINT UNSIGNED NOT NULL,
 		text TEXT NOT NULL,
 		time_stamp BIGINT UNSIGNED NOT NULL,
 		parent_id BIGINT UNSIGNED NOT NULL,
-		FROEIGN KEY (emotion_id) REFERENCES Emotion(id),
+		FOREIGN KEY (emotion_id) REFERENCES Emotion(id),
 		PRIMARY KEY (id)
 	)`,
-	`CREATE TABLE IF NOT EXISIS Traffic (
+	`CREATE TABLE IF NOT EXISTS Traffic (
 		id BIGINT UNSIGNED NOT NULL AUTO_INCREMENT,
 		emotion_id BIGINT UNSIGNED NOT NULL,
 		likes int UNSIGNED NOT NULL,
