62c7cee606ae6002b5d6705470d7027bdce7151d
Rock Chin <1010553892@qq.com>
2023-05-13 02:36:47 +0000

    perf: 优化emotion结构

--- pkg/models/qzone/entities/emotion.go
+++ pkg/models/qzone/entities/emotion.go
@@ -3,6 +3,7 @@ package qzone
 type Emotion struct {
 	ID        int64  // 数据库中的id
 	Eid       string // 说说id(腾讯系统)
+	QQ        int64  // 说说所属人的QQ号
 	PersonID  int64  // 说说所属人的id
 	Text      string // 说说文字内容
 	TimeStamp int64  // 说说发表时间戳
