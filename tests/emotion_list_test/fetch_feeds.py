4b406d3c947342192cd969ed8d2213ba09c317f1
Rock Chin <1010553892@qq.com>
2023-05-13 02:37:41 +0000

    chore: 说说列表协议测试脚本

---
+++ tests/emotion_list_test/fetch_feeds.py
@@ -0,0 +1,73 @@
+import yaml
+
+cookies = ""
+# 读取config.yaml中的qzone.cookies
+yaml_str = ""
+with open('config.yaml', 'r') as f:
+    yaml_str = f.read()
+
+config = yaml.load(yaml_str, Loader=yaml.FullLoader)
+cookies = config['qzone']['cookies']
+
+print(cookies)
+
+def generate_gtk(skey):
+    """生成gtk"""
+    hash_val = 5381
+    for i in range(len(skey)):
+        hash_val += (hash_val << 5) + ord(skey[i])
+    return str(hash_val & 2147483647)
+
+# 把cookies转换成字典
+cookies_dict = {}
+for cookie in cookies.split(';'):
+
+    cookie = cookie.strip()
+    cookie = cookie.split('=')
+    if len(cookie)<2:
+        continue
+    cookies_dict[cookie[0]] = cookie[1]
+
+print(cookies_dict)
+
+gtk = generate_gtk(cookies_dict['p_skey'])
+
+import time
+# 获取接口数据
+url = "https://user.qzone.qq.com/proxy/domain/ic2.qzone.qq.com/cgi-bin/feeds/feeds3_html_more?uin={}&scope=0&view=1&daylist=&uinlist=&gid=&flag=1&filter=all&applist=all&refresh=0&aisortEndTime=0&aisortOffset=0&getAisort=0&aisortBeginTime=0&pagenum=1&externparam=&firstGetGroup=0&icServerTime=0&mixnocache=0&scene=0&begintime=0&count=10&dayspac=0&sidomain=qzonestyle.gtimg.cn&useutf8=1&outputhtmlfeed=1&rd=0.3436615045444116&usertime={}&windowId=0.9554080071832363&getob=1&g_tk={}".format(cookies_dict['uin'][1:],time.time()*1000, gtk)
+
+import requests
+
+resp = requests.get(
+    url,
+    cookies=cookies_dict
+)
+
+# print(resp.text)
+
+# 提取所有nickname:'xxx',emoji中的xxx
+
+import re
+
+nicknames = re.findall(r"nickname:'(.*?)',emoji", resp.text)
+
+print(nicknames)
+
+# 提取所有,uin:'xxx',ouin中的xxx
+
+uins = re.findall(r",uin:'(.*?)',ouin", resp.text)
+
+print(uins)
+
+# 提取所有html:'xxx',中的xxx
+
+htmls = re.findall(r"html:'(.*?)',", resp.text)
+
+print(htmls)
+
+# 把htmls以json存到htmls.json中
+import json
+
+with open('htmls.json', 'w') as f:
+    json.dump(htmls, f, ensure_ascii=False, indent=4)
+
---
+++ tests/emotion_list_test/retrieve_info_from_htmls.py
@@ -0,0 +1,59 @@
+
+
+import json
+def retrieve_info_from_html(htmls: str):
+    """从html中提取信息"""
+    # 提取发表人uin和nick
+    # nameCard_12345678\\x22>RockChinQ\\x3C\\/a> 中的12345678和RockChinQ
+    import re
+    
+    uin_nick = re.findall(r"nameCard_(\d+)\\x22>(.*?)\\x3C\\/a>", htmls)
+
+    print(uin_nick)
+
+    # 提取文字内容
+    # class=\\x22f-info\\x22>This is content here!\\x3C\\/div>\\x3Cdiv class=\\x22qz_summary wupfeed\\x22 中的This is content here!
+
+    contents = re.findall(r"class=\\x22f-info\\x22>(.*?)\\x3C\\/div>\\x3Cdiv class=\\x22qz_summary wupfeed\\x22", htmls)
+
+    print(contents)
+
+    # 提取绝对时间戳
+    # data-abstime=\\x221683901591\\x22 中的1683901591
+
+    abstimes = re.findall(r"data-abstime=\\x22(\d+)\\x22", htmls)
+
+    print(abstimes)
+
+    # 提取图片
+    # src=\\x22http:\\/\\/a1.qpic.cnxxx style=中的http:\\/\\/a1.qpic.cnxxx
+    
+    pattern = re.compile(r"src=\\x22(http:\\/\\/a1.*?)\\x22")
+
+    images = pattern.findall(htmls)
+
+    # 把\\/替换成/
+    images = [image.replace('\\/', '/') for image in images]
+
+    # 把&amp;替换成&
+    images = [image.replace('&amp;', '&') for image in images]
+
+    # 提取每个image中的&ek=1前的字符串
+    images = [image.split('&ek=1')[0] for image in images]
+
+    print(json.dumps(images, indent=4, ensure_ascii=False))
+
+    # 提取tid
+    # data-tid=\\x22abcd1234\\x22 中的abcd1234
+
+    tids = re.findall(r"data-tid=\\x22(\w+)\\x22", htmls)
+
+    print(tids)
+
+
+# 读取htmls.json
+htmls = None
+with open('htmls.json', 'r', encoding='utf-8') as f:
+    htmls = json.load(f)
+
+retrieve_info_from_html(htmls[0])
